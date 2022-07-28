31a2602a5ba636a7187ac3adb1517fcfa960d954
truefusion <truefusion@gmail.com>
2022-07-27 23:05:19 -0400

    Update Timeline.vue
    Prototyping.

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,9 +1,19 @@
 <script setup>
+    import { useObjectStore } from './../stores/objects.js';
+
+    const objects = useObjectStore();
 </script>
 
 <template>
     <div id="timeline">
         <div class="toolbar">
+        </div>
+        <div class="objects">
+            <div class="object" v-for="obj in objects.ObjectList"  :key="obj.id">
+                <div class="left">
+                    {{obj.name}}
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -11,5 +21,21 @@
 <style lang="less">
     #timeline {
         @apply bg-gray-700 flex flex-col;
+
+        .objects {
+            @apply flex flex-col text-white;
+
+            .object {
+                @apply border-b border-gray-500 divide-gray-500;
+
+                > * {
+                    @apply p-2;
+                }
+
+                .left {
+                    @apply border-gray-400 border-r cursor-pointer w-80 hover:bg-gray-600;
+                }
+            }
+        }
     }
 </style>
